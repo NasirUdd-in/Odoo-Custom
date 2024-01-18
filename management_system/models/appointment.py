b36431d3b9eb6469157ac852eb6fef91e51e8d75
Nasirudd-in <nasir.diudevs@gmail.com>
2024-01-18 12:40:48 +0600

    one2many added

--- management_system/models/appointment.py
+++ management_system/models/appointment.py
@@ -18,6 +18,7 @@ class HospitalAppointment(models.Model):
         ('in_consultation', 'In Cousultation'),
         ('done', 'Done'),
         ('cancel', 'Canceled')], default='draft', string="Status", required=True)
+    pharmacy_line_ids = fields.One2many('appointment.pharmacy.lines','appointment_id', string='Pharmacy Lines')
     
     @api.onchange('patient_id')
     def onChange_patient_id(self):
@@ -35,3 +36,14 @@ class HospitalAppointment(models.Model):
     def action_cancel(self):
         for rec in self:
             rec.state = 'cancel'
+            
+            
+
+class AppointmentPharmacy(models.Model):
+    _name = "appointment.pharmacy.lines"
+    _description = "Appointment Pharmacy Lines"
+    
+    product_id = fields.Many2one('product.product', required=True)
+    price_unit = fields.Float(related='product_id.list_price')
+    qty = fields.Integer(string='Quantity', default=1)
+    appointment_id = fields.Many2one('hospital.appointment', string='Appointment')
